b0aba4edadaa9a00919ec02db7ebd3768a779b51
brira123 <brira.shafique@gmail.com>
2018-05-20 11:24:03 -1000

    transfer

---
+++ script.js
@@ -0,0 +1,12 @@
+const temperature = 36;
+let karachiTemperature = 40;
+
+console.log(karachiTemperature)
+
+if (karachiTemperature > 30) {
+    console.log("TOO HOT");
+} else if (karachiTemperature > 10) {
+    console.log("MODERATE");
+} else if (karachiTemperature < 10) {
+    console.log("TOO COLD");
+}
